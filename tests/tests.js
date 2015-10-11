bac49c688b964b78c75dc352ed88972ac4708e19
alu0100706734 <alu0100706734@ull.edu.es>
2015-10-11 21:01:33 +0200

    añadiendo pruebas al test

---
+++ tests/tests.js
@@ -0,0 +1,42 @@
+var assert = chai.assert;
+
+suite('temperature', function() {
+    test('32F = 0C', function() {
+        original.value = "32F";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+    });
+    test('45C = 113.0 Farenheit', function() {
+        original.value = "45C";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+    });
+    test('5X = error', function() {
+        original.value = "5X";
+        calculate();
+        assert.match(converted.innerHTML, /ERROR/);
+    });
+	
+	test('14.32 = error', function(){
+		original.value = "14.32";
+		calculate();
+		assert.notEqual(converted.innerHTML, 14.32, "/ERROR/");
+	});
+	test('Ojo viruelo = error', function(){
+		original.value = "Ojo viruelo";
+		calculate();
+		assert.equal(converted.innerHTML, 'ERROR! Prueba algo como \'89.5F\' ',/ERROR/);
+	});
+});
+
+describe('Array', function(){
+  before(function(){
+    // ...
+  });
+
+  describe('#indexOf()', function(){
+    it('should return -1 when not present', function(){
+      [1,2,3].indexOf(4).should.equal(-1);
+    });
+  });
+});
