ef3abf935186ba5a0811a2b0dee5c98f7f799bda
alu0100706734 <alu0100706734@ull.edu.es>
2015-10-13 16:28:31 +0200

    modificando cambio de clases

--- temperature.js
+++ temperature.js
@@ -1,4 +1,66 @@
-"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+
+function Medida(){
+	this.value = null;
+	this.type = null;
+}
+
+function Temperatura(valor){
+	this.valor = valor;
+}
+
+Temperatura.prototype = new Medida();
+Temperatura.prototype.constructor = Temperatura;
+
+Temperatura.prototype.converted_ = function (converted){
+  var temp = original.value;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([cCkKfF])/; //cambiar con exponente
+  
+  var m = temp.match(regexp);
+  
+  if (m) {
+    var num = m[1];
+    var type = m[2];
+    num = parseFloat(num); 
+	
+		    if (converted == 'c' || converted == 'C') {
+      resultF = (num * 9/5)+32;
+      resultF = resultF.toFixed(1)+" Farenheit  |";
+	  resultK = (num + 273.15);
+	  resultK = resultK.toFixed(1)+" Kelvin   |";
+	  resultC = num;
+	  resultC = resultC.toFixed(1)+" Celsius   |";
+	  
+    }
+    else if (converted == 'f' || converted =='F'){
+      resultC = (num - 32)*5/9;
+      resultC = resultC.toFixed(1)+" Celsius   |";
+	  resultK = (num + 459.67)*5/9;
+	  resultK = resultK.toFixed(1)+" Kelvin   |";
+	  resultF = num;
+	  resultF = resultF.toFixed(1)+" Farenheit   |";
+    }
+	else if (converted == 'k' || converted == 'K'){
+		resultF = num * 9/5 - 459.67;
+		resultF = resultF.toFixed(1)+" Farenheit   |";
+		resultC = num - 273.15;
+		resultC = resultC.toFixed(1)+" Celsius   |";
+		resultK = num;
+		resultK = resultK.toFixed(1)+" Kelvin   |";
+	}
+  }
+}
+
+function conversorHTML(converted){
+	input = document.getElementById("original")
+}
+
+
+
+
+
+
+
+/*"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calculate() {
   var resultF;
   var resultC;
@@ -49,4 +111,4 @@ function calculate() {
   else {
     converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
   }
-}
+}*/
